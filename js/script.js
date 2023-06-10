61d50a905c0cefe87cd82680c13163e2d83542a5
Rivelon <rivelon22@gmail.com>
2023-06-10 18:18:30 +0300

    create modal window

--- js/script.js
+++ js/script.js
@@ -42,11 +42,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const deadline = "2023-06-11";
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      (days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((t / 1000 / 60) % 60)),
+        (seconds = Math.floor((t / 1000) % 60));
+    }
 
     return {
       total: t,
@@ -89,4 +98,21 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   setClock(".timer", deadline);
+
+  //modal
+  const modalTrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  modalTrigger.addEventListener('click', () => {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = 'hidden';
+  });
+
+  modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = '';
+  });
 });
